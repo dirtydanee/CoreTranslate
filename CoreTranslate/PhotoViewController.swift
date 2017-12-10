dffde841eabe6159c9d954f94198acf7340c8f51
Daniel Metzing <daniel.metzing@centralway.com>
2017-12-10 17:26:34 +0100

    Initial commit

---
+++ CoreTranslate/PhotoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoViewController.swift
+//  CoreTranslate
+//
+//  Created by Daniel.Metzing on 10.12.17.
+//  Copyright © 2017 Dirtylabs. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+}
+
